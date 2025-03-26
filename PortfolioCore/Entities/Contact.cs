79ed7634926a6664c80f434248c77754336c3245
Sercan TOYGUN <sercan.toygun41@gmail.com>
2025-03-26 22:34:23 +0300

    Add project files.

---
+++ PortfolioCore/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace PortfolioCore.Entities
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public String Address { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Email { get; set; }
+        public String Location { get; set; }
+    }
+}
---
+++ PortfolioCore/Entities/Education.cs
@@ -0,0 +1,14 @@
+﻿namespace PortfolioCore.Entities
+{
+    public class Education
+    {
+        //Field, Variable, Property
+       // int x; // class içinde field
+        public int EducationId { get; set; }
+        public string Title { get; set; }
+        public string DateInfo { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+
+    }
+}
