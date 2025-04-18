b4f0fff1aee91d45c9a15f6b315c2e4e592d5d8f
Sercan TOYGUN <sercan.toygun41@gmail.com>
2025-04-18 21:59:45 +0300

    Css ve JS kodları eklendi ve Component yapısına başlandı

---
+++ PortfolioCore/Models/ViewComponents/_DefaultHeadComponentsPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCore.Models.ViewComponents
+
+{
+    public class _DefaultHeadComponentsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
