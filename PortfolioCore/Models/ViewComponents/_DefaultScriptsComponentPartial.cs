edbba4dead995d446be12dffc6a6f54403bed129
Sercan TOYGUN <sercan.toygun41@gmail.com>
2025-04-26 21:38:46 +0300

    Portfolio controller tamamlandı

---
+++ PortfolioCore/Models/ViewComponents/_DefaultScriptsComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCore.Models.ViewComponents
+{
+
+    public class _DefaultScriptsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
